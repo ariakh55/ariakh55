980065372ce0adbf231f48334d8cea1e0916e098
Aria KH <khoshnoodaria@gmail.com>
2021-01-21 00:09:51 +0330

    Updates

---
+++ precache-manifest.7bad03c32e1977876e55ea08c76e8f20.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88a0d5688efe8746aa03b1b1ec5e4d43",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1bb7419d384edd95c017",
+    "url": "/static/css/2.1b4fbf1f.chunk.css"
+  },
+  {
+    "revision": "1bb7419d384edd95c017",
+    "url": "/static/js/2.c565bf1c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.c565bf1c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3940d656ddf972612247",
+    "url": "/static/js/main.ab8cc02a.chunk.js"
+  },
+  {
+    "revision": "770344394e9f4453d0a9",
+    "url": "/static/js/runtime-main.3a907230.js"
+  },
+  {
+    "revision": "acc002a83a446e66b65b02379159f0e2",
+    "url": "/static/media/AutoSignUp.acc002a8.png"
+  },
+  {
+    "revision": "abac335e95fc08a9df62fc46395bc236",
+    "url": "/static/media/KallehSP.abac335e.png"
+  },
+  {
+    "revision": "8267b4a8b1c125cd2f66b6cfb971715e",
+    "url": "/static/media/eval.8267b4a8.png"
+  },
+  {
+    "revision": "919e86d98a447fa7abaf8d98393ea29e",
+    "url": "/static/media/messenger.919e86d9.png"
+  }
+]);
