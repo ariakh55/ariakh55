85132106a1400c375e74d8a56a66df7f512757ab
Aria KH <khoshnoodaria@gmail.com>
2024-06-06 14:27:29 +0330

    remove unsused scripts

--- public/scripts/markdown.js
+++
@@ -1,9 +0,0 @@
-function renderMarkdown({ detail }) {
-  detail.serverResponse = window
-    .markdownit({
-      html: true,
-      // breaks: true,
-      typographer: true,
-    })
-    .render(detail.serverResponse);
-}
